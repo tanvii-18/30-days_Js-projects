074beae35ca3a7a0ce6d0871048d799ef998e6d7
Tanvi Patel <8438tanvipatel@gmail.com>
2025-04-13 22:12:31 +0530

    Update script.js

--- Quiz web/script.js
+++ Quiz web/script.js
@@ -3,12 +3,14 @@ $(document).ready(function (){
     
     let quizData = [];
     let currentQue = 0;
+    let count = 60;
 
 
     $.getJSON("ques.json", function(data){
         quizData = data;
         showQuestions();
         showOption();
+        startTimer();
     })
 
     const showQuestions = () =>{
@@ -22,6 +24,11 @@ $(document).ready(function (){
             $(".options-cont").append( `<button class="option-btn">${options}</button>`);
        });
     }
+
+        let startTimer = () => setInterval(() => {
+            count--;
+            $(".timer").html("Time Left : "+ count + "s")
+        }, 1000); 
 
     $("#next-que").on("click", function(){
         if(currentQue < quizData.length -1){
